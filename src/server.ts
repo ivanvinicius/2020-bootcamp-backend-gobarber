cb0f25ee18e1169b5aafc1eb8cdd5320650ddc3a
ivanvinicius <ivanvinicius88@gmail.com>
2020-05-21 20:11:15 -0300

    cadastrando e listando appointments

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,13 @@
 import express from 'express';
 
 import logRequest from './utils/logRequest';
+import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
 app.use(logRequest);
-
-app.get('/', (request, response) => response.send('hello worldsssss'));
+app.use(routes);
 
 /* eslint-disable no-console */
-app.listen(3333, () => console.log('🚀 Server is running!'));
+app.listen(3333, () => console.log('🚀 Server is running on port:3333!'));
