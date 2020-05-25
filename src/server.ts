85433b2ef1952bc3e03174ed339d823e67f02a6e
ivanvinicius <ivanvinicius88@gmail.com>
2020-05-25 15:03:29 -0300

    servindo arquivos estáticos

--- src/server.ts
+++ src/server.ts
@@ -6,11 +6,13 @@ import './database';
 
 import logRequest from './utils/logRequest';
 import routes from './routes';
+import uploadConfig from './config/upload';
 
 const app = express();
 
 app.use(express.json());
 app.use(logRequest);
+app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
 
 /* eslint-disable no-console */
