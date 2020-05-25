b4328e33be7c0132ecff3e95cc0e9182d4cb11fc
ivanvinicius <ivanvinicius88@gmail.com>
2020-05-25 14:28:49 -0300

    atualizando avatar

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,5 +1,8 @@
 import { Router } from 'express';
 import { getRepository } from 'typeorm';
+import multer from 'multer';
+
+import uploadConfig from '../config/upload';
 
 import User from '../models/User';
 import CreateUsersService from '../services/CreateUserService';
@@ -7,6 +10,7 @@ import CreateUsersService from '../services/CreateUserService';
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
 /** This route gonna be deleted */
 usersRouter.get('/', ensureAuthenticated, async (request, response) => {
@@ -36,5 +40,16 @@ usersRouter.post('/', async (request, response) => {
     return response.status(400).json({ error: err.message });
   }
 });
+
+usersRouter.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  async (request, response) => {
+    console.log(request.file);
+
+    return response.json({ message: 'Profile picture was uploaded.' });
+  },
+);
 
 export default usersRouter;
