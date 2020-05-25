2721429cbc846488ffc9789fcdac29d676818f60
ivanvinicius <ivanvinicius88@gmail.com>
2020-05-25 15:56:26 -0300

    lidando com erros

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,8 +1,5 @@
 import { Router } from 'express';
-import { getRepository } from 'typeorm';
 import multer from 'multer';
-
-import User from '../models/User';
 
 import CreateUsersService from '../services/CreateUserService';
 import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
@@ -14,33 +11,20 @@ import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 const usersRouter = Router();
 const upload = multer(uploadConfig);
 
-/** This route gonna be deleted */
-usersRouter.get('/', ensureAuthenticated, async (request, response) => {
-  const userRepository = getRepository(User);
-
-  const users = await userRepository.find();
-
-  return response.json(users);
-});
-
 usersRouter.post('/', async (request, response) => {
-  try {
-    const { name, email, password } = request.body;
+  const { name, email, password } = request.body;
 
-    const createUser = new CreateUsersService();
+  const createUser = new CreateUsersService();
 
-    const user = await createUser.execute({
-      name,
-      email,
-      password,
-    });
+  const user = await createUser.execute({
+    name,
+    email,
+    password,
+  });
 
-    delete user.password;
+  delete user.password;
 
-    return response.json(user);
-  } catch (err) {
-    return response.status(400).json({ error: err.message });
-  }
+  return response.json(user);
 });
 
 usersRouter.patch(
@@ -48,20 +32,16 @@ usersRouter.patch(
   ensureAuthenticated,
   upload.single('avatar'),
   async (request, response) => {
-    try {
-      const updateUserAvatar = new UpdateUserAvatarService();
+    const updateUserAvatar = new UpdateUserAvatarService();
 
-      const user = await updateUserAvatar.execute({
-        user_id: request.user.id,
-        avatarFileName: request.file.filename,
-      });
+    const user = await updateUserAvatar.execute({
+      user_id: request.user.id,
+      avatarFileName: request.file.filename,
+    });
 
-      delete user.password;
+    delete user.password;
 
-      return response.json(user);
-    } catch (err) {
-      return response.status(400).json({ error: err.message });
-    }
+    return response.json(user);
   },
 );
 
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,15 @@
 import 'reflect-metadata';
 
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+
+import 'express-async-errors';
 
 import './database';
 
 import logRequest from './utils/logRequest';
 import routes from './routes';
 import uploadConfig from './config/upload';
+import AppError from './errors/AppError';
 
 const app = express();
 
@@ -14,6 +17,24 @@ app.use(express.json());
 app.use(logRequest);
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
+
+app.use(
+  (error: Error, request: Request, response: Response, _next: NextFunction) => {
+    if (error instanceof AppError) {
+      return response.status(error.statusCode).json({
+        status: 'error',
+        messsage: error.message,
+      });
+    }
+
+    console.log(error);
+
+    return response.status(500).json({
+      status: 'error',
+      message: 'Internal server error.',
+    });
+  },
+);
 
 /* eslint-disable no-console */
 app.listen(3333, () => console.log('🚀 Server is running on port:3333!'));
