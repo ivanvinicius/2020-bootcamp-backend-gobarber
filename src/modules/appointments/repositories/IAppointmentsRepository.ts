b72d5b48df732343898405c5679ff2bb90612c1c
ivanvinicius <ivanvinicius88@gmail.com>
2020-06-29 19:31:13 -0300

    reescrevendo repositórios

--- src/modules/appointments/repositories/IAppointmentsRepository.ts
+++ src/modules/appointments/repositories/IAppointmentsRepository.ts
@@ -1,5 +1,9 @@
 import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
 
+import ICreateAppointmentDTO from '@modules/appointments/dtos/ICreateAppointmentDTO';
+
 export default interface IAppointmentsRepository {
+  create(date: ICreateAppointmentDTO): Promise<Appointment>;
+
   findByDate(date: Date): Promise<Appointment | undefined>;
 }
