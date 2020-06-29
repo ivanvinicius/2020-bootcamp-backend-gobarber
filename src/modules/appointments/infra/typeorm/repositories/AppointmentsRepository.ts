c9e951d18353a66616c966701a0a8ff3e16d4042
ivanvinicius <ivanvinicius88@gmail.com>
2020-06-29 19:16:06 -0300

    liskov substitution principle

--- src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
+++ src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
@@ -2,17 +2,17 @@ import { EntityRepository, Repository } from 'typeorm';
 
 import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
 
-interface FindaDateDTO {
-  date: Date;
-}
+import IAppointmentRepository from '@modules/appointments/repositories/IAppointmentsRepository';
+
 @EntityRepository(Appointment)
-class AppointmentsRepository extends Repository<Appointment> {
-  public async findByDate({ date }: FindaDateDTO): Promise<Appointment | null> {
+class AppointmentsRepository extends Repository<Appointment>
+  implements IAppointmentRepository {
+  public async findByDate(date: Date): Promise<Appointment | undefined> {
     const findAppointment = await this.findOne({
       where: { date },
     });
 
-    return findAppointment || null;
+    return findAppointment;
   }
 }
 
--- src/modules/appointments/services/CreateAppointmentService.ts
+++ src/modules/appointments/services/CreateAppointmentService.ts
@@ -4,7 +4,7 @@ import { getCustomRepository } from 'typeorm';
 import AppError from '@shared/errors/AppError';
 
 import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
-import AppointmentRepository from '../repositories/AppointmentsRepository';
+import AppointmentRepository from '@modules/appointments/infra/typeorm/repositories/AppointmentsRepository';
 
 interface Request {
   provider_id: string;
@@ -17,9 +17,9 @@ class CreateAppointmentService {
 
     const appointmentDate = startOfHour(date);
 
-    const findAppointmentInSameDate = await appointmentRepository.findByDate({
-      date: appointmentDate,
-    });
+    const findAppointmentInSameDate = await appointmentRepository.findByDate(
+      appointmentDate,
+    );
 
     if (findAppointmentInSameDate) {
       throw new AppError('This appointment is already booked.');
