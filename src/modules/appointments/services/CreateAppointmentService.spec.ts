6fd238d172c8f44d96721eb35871436bd689b3e3
ivanvinicius <ivanvinicius88@gmail.com>
2020-07-03 14:29:01 -0300

    pensando nos testes

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,3 +1,11 @@
-test('sum tow numbers', () => {
-  expect(1 + 2).toBe(3)
-});
+import CreateAppointmentService from './CreateAppointmentService';
+
+describe('CreateAppointment', () => {
+  it('should be able to create a new appointment', () => {
+    expect('').toBe('')
+  })
+
+  // it('should not be able to create a new appointment on the same time', () => {
+  //   expect('').toBe('');
+  // })
+})
