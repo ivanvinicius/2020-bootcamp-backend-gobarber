86b7a5fe0f727b8a1bacb68ed41cb9837802a472
ivanvinicius <ivanvinicius88@gmail.com>
2020-07-15 21:17:58 -0300

    agenda do prestado e conexao mongodb

---
+++ src/modules/appointments/services/ListProviderAppointmentsService.spec.ts
@@ -0,0 +1,42 @@
+// import AppError from '@shared/errors/AppError';
+
+import FakeAppointmentsRepository from '@modules/appointments/repositories/fakes/FakeAppointmentsRepository';
+import ListProviderAppointmentsService from '@modules/appointments/services/ListProviderAppointmentsService';
+
+let fakeAppointmentsRepository: FakeAppointmentsRepository;
+let listProviderAppointmentsService: ListProviderAppointmentsService;
+
+describe('ListProviderAppointments', () => {
+  beforeEach(() => {
+    fakeAppointmentsRepository = new FakeAppointmentsRepository();
+
+    listProviderAppointmentsService = new ListProviderAppointmentsService(
+      fakeAppointmentsRepository,
+    );
+  });
+
+  it('should be able to list the provider appointments on specific day', async () => {
+    const appointment1 = await fakeAppointmentsRepository.create({
+      provider_id: 'provider',
+      user_id: 'user',
+      date: new Date(2020, 6, 16, 8, 0, 0),
+    });
+
+    const appointment2 = await fakeAppointmentsRepository.create({
+      provider_id: 'provider',
+      user_id: 'user',
+      date: new Date(2020, 6, 16, 9, 0, 0),
+    });
+
+    const availability = await listProviderAppointmentsService.execute({
+      provider_id: 'provider',
+      day: 16,
+      month: 7,
+      year: 2020,
+    });
+
+    expect(availability).toEqual(
+      expect.arrayContaining([appointment1, appointment2]),
+    );
+  });
+});
